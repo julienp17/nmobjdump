40606c2c1aaf8abd0e2d2f79d76ab70d8e4f3c39
Julien PAUSE <julien.pause@epitech.eu>
2021-03-09 10:41:49 +0400

    Add better conditions for section printing

--- objdump/src/dump.c
+++ objdump/src/dump.c
@@ -8,8 +8,11 @@
 #include "my_objdump.h"
 
 static void dump_section(const elf_t *elf, size_t i);
-static bool should_print_section(const Elf64_Shdr *shdr);
 static void print_ascii(const unsigned char *content, size_t j, size_t size);
+static bool should_print_section(const Elf64_Shdr *shdr,
+                                const char *sec_name,
+                                const uchar *content);
+static bool section_is_empty(const uchar *content, const size_t size);
 
 void dump(const elf_t *elf)
 {
@@ -21,30 +24,31 @@ void dump(const elf_t *elf)
     dump_flags(flags);
     printf("start address 0x%016lx\n\n", elf->ehdr->e_entry);
     for (size_t i = 0; i < elf->ehdr->e_shnum; i++)
-        if (should_print_section(&(elf->shdr[i])))
-            dump_section(elf, i);
+        dump_section(elf, i);
 }
 
 static void dump_section(const elf_t *elf, size_t i)
 {
-    char *sec_name = (char *)(elf->strtable + elf->shdr[i].sh_name);
-    unsigned char *content =
-        (unsigned char *)((char *)elf->ehdr + elf->shdr[i].sh_offset);
+    Elf64_Shdr *shdr = &(elf->shdr[i]);
+    char *sec_name = (char *)(elf->strtable + shdr->sh_name);
+    uchar *content = (uchar *)((char *)elf->ehdr + shdr->sh_offset);
 
+    if (!should_print_section(shdr, sec_name, content))
+        return;
     printf("Contents of section %s:", sec_name);
-    for (size_t j = 0 ; j < elf->shdr[i].sh_size ; j++) {
+    for (size_t j = 0 ; j < shdr->sh_size ; j++) {
         if (j % 16 == 0)
-            printf("\n %04lx ", j);
+            printf("\n %04x ", (uint32_t)(shdr->sh_addr + j));
         printf("%02x", content[j]);
         if ((j + 1) % 4 == 0)
             printf(" ");
-        if ((j + 1) % 16 == 0 || j + 1 == elf->shdr[i].sh_size)
-            print_ascii(content, j, elf->shdr[i].sh_size);
+        if ((j + 1) % 16 == 0 || j + 1 == shdr->sh_size)
+            print_ascii(content, j, shdr->sh_size);
     }
     printf("\n");
 }
 
-static void print_ascii(const unsigned char *content, size_t j, size_t size)
+static void print_ascii(const uchar *content, size_t j, size_t size)
 {
     size_t start = 16 * (j / 16);
 
@@ -64,14 +68,25 @@ static void print_ascii(const unsigned char *content, size_t j, size_t size)
     }
 }
 
-static bool should_print_section(const Elf64_Shdr *shdr)
+static bool should_print_section(const Elf64_Shdr *shdr,
+                                const char *sec_name,
+                                const uchar *content)
 {
+    if ((shdr->sh_type == SHT_RELA || shdr->sh_type == SHT_REL))
+        return !section_is_empty(content, shdr->sh_size);
     return (
         shdr->sh_size > 0
         && shdr->sh_type != SHT_NOBITS
         && shdr->sh_type != SHT_SYMTAB
-        && shdr->sh_type != SHT_RELA
-        && shdr->sh_type != SHT_REL
-        && shdr->sh_type != SHT_STRTAB
+        && strcmp(sec_name, ".strtab") != 0
+        && strcmp(sec_name, ".shstrtab") != 0
     );
+}
+
+static bool section_is_empty(const uchar *content, const size_t size)
+{
+    for (size_t j = 0 ; j < size ; j++)
+        if (isprint(content[j]) && content[j] != ' ')
+            return false;
+    return true;
 }
